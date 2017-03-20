cb00196ef1d75a14a4356f135b7719ea6727242e
tim <jan-brenner@gmx.de>
2017-03-20 16:30:44 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+TestDataGridViewWithWrapMode
+====================================
+
+TestDataGridViewWithWrapMode is an executable to check how a DataGridView with wrap mode works.
+The executable was written and tested in .Net 4.6.2.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/n5n4ehlo4q0w6st9?svg=true)](https://ci.appveyor.com/project/SeppPenner/testdatagridviewwithwrapmode)
+
+
+Change history
+--------------
+
+* **Version 1.0.0.1 (2017-03-20)** : Switched to .Net to 4.6.2, Refactored code.
+* **Version 1.0.0.0 (2017-03-20)** : 1.0 release.
---
+++ TestDataGridViewWithWrapMode/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("TestDataGridViewWithWrapMode")>
+<Assembly: AssemblyDescription("TestDataGridViewWithWrapMode")>
+<Assembly: AssemblyCompany("Hämmer Electronics")>
+<Assembly: AssemblyProduct("TestDataGridViewWithWrapMode")>
+<Assembly: AssemblyCopyright("Copyright © Hämmer Electronics")>
+<Assembly: AssemblyTrademark("TestDataGridViewWithWrapMode")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("680d9268-761a-4bbc-ae42-45d675701798")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
