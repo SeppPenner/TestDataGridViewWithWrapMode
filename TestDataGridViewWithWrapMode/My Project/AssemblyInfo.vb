a21a990ec8b40a9274ea7f0ae0a63fd7d1edd124
SeppPenner <HansM2013@gmx.de>
2019-10-27 22:40:27 +0100

    Updated nuget packages, added GitVersionTask.

--- TestDataGridViewWithWrapMode/My Project/AssemblyInfo.vb
+++ TestDataGridViewWithWrapMode/My Project/AssemblyInfo.vb
@@ -18,17 +18,3 @@ Imports System.Runtime.InteropServices
 
 'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 <Assembly: Guid("680d9268-761a-4bbc-ae42-45d675701798")>
-
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Hauptversion
-'      Nebenversion 
-'      Buildnummer
-'      Revision
-'
-' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-' übernehmen, indem Sie "*" eingeben:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("1.0.0.2")>
-<Assembly: AssemblyFileVersion("1.0.0.2")>
